81f35c0aa4f06d5d76ca4edee7bd52622696b319
Luke Thompson <lpthompson92@gmail.com>
2019-06-23 11:29:07 +1000

    Add e2e tests and update readme

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,31 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
+describe('CRUDing todos', () => {
+  it('can add a new todo', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.get('.add-todo input[type="text"]').type('Sample todo')
+    cy.get('.add-todo button[type="submit"]').click()
+    cy.get('.todos-view__content .todo-item__title').contains('Sample todo')
+  })
+
+  it('can mark a todo as complete and remove the todo from curernt todos', () => {
+    cy.get('.todos-view__content .todo-item .input-checkbox').click()
+    cy.wait(1000).get('.todos-view__content .todo-item').should('not.to.exist')
+  })
+
+  it('can nagivate to completed todos and show the completed todo', () => {
+    cy.get('.tab-item:not(.tab-item--active)').click()
+    cy.get('.todos-view__content .todo-item__title').contains('Sample todo')
+  })
+
+  it('can edit the title of a todo', () => {
+    cy.get('.todos-view__content .todo-item__title').click()
+    cy.get('.edit-todo input[type="text"]').type(' edited')
+    cy.get('.edit-todo button[type="submit"]').click()
+    cy.get('.todos-view__content .todo-item__title').contains('Sample todo edited')
+  })
+
+  it('can delete a todo', () => {
+    cy.get('.todos-view__content .todo-item__title').click()
+    cy.get('.edit-todo .btn--warning').click()
+    cy.get('.todos-view__content').should('not.to.exist')
   })
 })
