076a6ea8753cdf2d1a6307629e37f067166430bc
Luke Thompson <lpthompson92@gmail.com>
2019-06-24 17:01:03 +1000

    Update e2e tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -6,12 +6,12 @@ describe('CRUDing todos', () => {
     cy.get('.todos-view__content .todo-item__title').contains('Sample todo')
   })
 
-  it('can mark a todo as complete and remove the todo from curernt todos', () => {
+  it('can mark a todo as complete and remove the todo from current todos', () => {
     cy.get('.todos-view__content .todo-item .input-checkbox').click()
     cy.wait(1000).get('.todos-view__content .todo-item').should('not.to.exist')
   })
 
-  it('can nagivate to completed todos and show the completed todo', () => {
+  it('can navigate to completed todos and show the completed todo', () => {
     cy.get('.tab-item:not(.tab-item--active)').click()
     cy.get('.todos-view__content .todo-item__title').contains('Sample todo')
   })
@@ -20,6 +20,17 @@ describe('CRUDing todos', () => {
     cy.get('.todos-view__content .todo-item__title').click()
     cy.get('.edit-todo input[type="text"]').type(' edited')
     cy.get('.edit-todo button[type="submit"]').click()
+    cy.get('.todos-view__content .todo-item__title').contains('Sample todo edited')
+  })
+
+  it('can edit a todo and change the completed flag', () => {
+    cy.get('.todos-view__content .todo-item__title').click()
+    cy.get('.edit-todo input[type="checkbox"]').click()
+    cy.get('.edit-todo button[type="submit"]').click()
+  })
+
+  it('can navigate to current todos and show the current todo', () => {
+    cy.get('.tab-item:not(.tab-item--active)').click()
     cy.get('.todos-view__content .todo-item__title').contains('Sample todo edited')
   })
 
